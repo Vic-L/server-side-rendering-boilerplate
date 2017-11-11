aac99b250d2ce3827bb0176abb15421dfdd7bb3a
Vic-L <vljc17@gmail.com>
2017-11-11 09:55:08 +0800

    feat: render home and all other pages in 2 functions

--- html/handler.js
+++ html/handler.js
@@ -1,8 +1,18 @@
 'use strict';
 
-module.exports.render = (event, context) => {
-  var html = '<html><head><title>One piece rox!</title></head>' + 
+module.exports.render = (event, context, callback) => {
+  console.log("event.stage", event.stage)
+  console.log("context", context)
+  const html = '<html><head><title>One piece rox!</title></head>' + 
         '<body><h1>One piece rox!</h1></body></html>'
-    
-    context.succeed(html)
+
+  const response = {
+    statusCode: 200,
+    headers: {
+      'Content-Type': 'text/html',
+    },
+    body: html,
+  };
+
+  callback(null, response);
 };
