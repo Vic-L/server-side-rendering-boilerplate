594e6c5e91a4ea81b6a1adcf0851f951cf5e2316
Vic-L <vljc17@gmail.com>
2017-12-21 20:19:42 +0800

    feat: add meta description, google site verification to html handler

--- html/handler.js
+++ html/handler.js
@@ -1,10 +1,17 @@
 'use strict';
 
 module.exports.render = (event, context, callback) => {
-  console.log("event.stage", event.stage)
-  console.log("context", context)
-  const html = '<html><head><title>One piece rox!</title></head>' + 
-        '<body><h1>One piece rox!</h1></body></html>'
+  const html = '<!DOCTYPE html><html><head>' + 
+    '<meta name="google-site-verification" content="' + process.env.GOOGLE_SITE_VERIFICATION + '" />' + 
+    '<meta name="viewport" content="width=device-width, initial-scale=1">' +
+    '<meta http-equiv="Content-Type" content="text/html;charset=utf-8">' +
+    '<title>One piece rox!</title>' +
+    '<meta name="description" content="One piece rox!">' +
+    '</head>' + 
+    '<body>' +
+    '<div id="app"></div>' +
+    '<script type="text/javascript" src="some js url" async></script>' +
+    '</body></html>'
 
   const response = {
     statusCode: 200,
